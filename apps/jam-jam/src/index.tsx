040b9e1cd46e0d4c79126ace10e11faa9a61e1d1
Kacper Woźniak <kacper.wozniak@billennium.com>
2021-10-05 22:51:18 +0200

    Add PWA

--- apps/jam-jam/src/index.tsx
+++ apps/jam-jam/src/index.tsx
@@ -7,9 +7,13 @@ import { App } from "./App";
 
 import "./i18n";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <Suspense fallback={<LoadingLanguageFallback />}>
     <App />
   </Suspense>,
   document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
